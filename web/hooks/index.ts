5f7f1a164322591d4ec67fbb523ad5efb838e4b3
hirokr <hirokr0625@gmail.com>
2026-05-21 02:01:42 +0600

    Remove unused hooks and clean up exports in the hooks directory

--- web/hooks/index.ts
+++ web/hooks/index.ts
@@ -1,6 +1,2 @@
-export * from "./useActiveTab";
-export * from "./useAiProcessingState";
-export * from "./useAvatarStudioNotification";
+
 export * from "./useDebounce";
-export * from "./useSettingsPreferences";
-export * from "./useStyleDiscovery";
